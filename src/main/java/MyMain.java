e3d2f40debe494a9cbdfa1612590ff6465659e86
nbutcher1 <22nbutcher@athenian.org>
2020-09-15 01:33:03 +0000

    done

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,27 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int lengths = binary.length();
+        int total = 0;
+        int x = 1;
+        for (int i = 0; i < lengths; i ++) {
+            if (binary.charAt(lengths-i - 1) == '1') {
+                if (i == 0) {
+                    total += 1;
+                }
+                else {
+                    total += x;
+                }
+                
+            }
+            x *= 2;
+
+        }
+        return total;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println(binaryToDecimal("1111"));
     }
 }
